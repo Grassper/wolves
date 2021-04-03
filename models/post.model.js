ba7c4284e14cb92a1867ae7ab2c7e775602939ee
Grassper <rishi.bharath27@gmail.com>
2021-04-04 00:29:13 +0530

    designed models, completed cred operation for post

---
+++ models/post.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  comments: [commentSchema],
+});
+
+
+
+module.exports = mongoose.model("post", postSchema);
--- routes/social.routes.js
+++ routes/social.routes.js
@@ -1,5 +1,83 @@
 const express = require("express");
 const router = express.Router();
 
+// importing models
+const postModel = require("../models/post.model");
 
-module.exports = router
+// getting all post
+router.get("/", async (req, res) => {
+  try {
+    const posts = await postModel.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// getting single post
+router.get("/:id", getPostById, (req, res) => {
+  res.send(res.post);
+});
+
+// creating one
+router.post("/", async (req, res) => {
+  const post = new postModel({
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+  });
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// updating one
+router.patch("/:id", getPostById, async (req, res) => {
+  if (req.body.title != null) {
+    res.post.title = req.body.title;
+  }
+  if (req.body.description != null) {
+    res.post.description = req.body.description;
+  }
+  if (req.body.imageUrl != null) {
+    res.post.imageUrl = req.body.imageUrl;
+  }
+
+  try {
+    const updatedPost = await res.post.save();
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// deleting one
+router.delete("/:id", getPostById, async (req, res) => {
+  try {
+    await res.post.remove();
+    res.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get post by id
+async function getPostById(req, res, next) {
+  let post;
+  try {
+    post = await postModel.findById(req.params.id);
+    if (post == null) {
+      return res.status(404).json({ message: "Cannot find post" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.post = post;
+  next();
+}
+
+module.exports = router;
