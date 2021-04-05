891559b001e9fb2e72dc4b5b85fd66e2d92a6710
Grassper <rishi.bharath27@gmail.com>
2021-04-05 22:21:35 +0530

    bugs fixed

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const path = require("path");
+const compression = require("compression");
 // importing logger
 const morgan = require("morgan");
 
@@ -24,6 +25,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 // initialing middlewares
+app.use(compression());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("combined"));
@@ -33,7 +35,17 @@ app.use(express.json());
 const socialRoutes = require("./routes/social.routes");
 const authRoutees = require("./routes/auth.routes");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.use("/api", socialRoutes);
 app.use("/users", authRoutees);
 
-app.listen(process.env.PORT, () => console.log("server started"));
+app.listen(process.env.PORT, () =>
+  console.log("server started " + process.env.PORT)
+);
